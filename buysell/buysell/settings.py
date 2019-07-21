e384c45af5488a539c86982bd49bf89f3a326fdb
Kanchan <kmahajan331@gmail.com>
2019-07-21 22:47:55 +0200

    Updating to python3 django2 and updating urls and views

--- buysell/buysell/settings.py
+++ buysell/buysell/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'vg=5$2^^d5gt!afvu*qjb_sd**2b24lcgm%$4*(%g$ztqzbz1('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
